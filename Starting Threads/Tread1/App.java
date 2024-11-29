f0eac6729473319210e2321bf9adf9f8d37bf0f8
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-11-29 01:41:52 +0100

    Update App.java

--- Starting Threads/Tread1/App.java
+++ Starting Threads/Tread1/App.java
@@ -1,1 +1,27 @@
+package Starting_Threads;
 
+class Runner extends Thread {
+    public void run() {
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello" + i);
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                //throw new RuntimeException(e);
+
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
+
+
+public class App1 {
+    public static void main(String[] args) {
+        Runner runner1 = new Runner();
+        runner1.start();
+    }
+}
